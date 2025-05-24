e33542efb9c6afdb8833c05514833d9282617d64
Naruto999-bit <DUSOROMRIDHA@gmail.com>
2025-05-24 08:19:40 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
-  ownerNumber: "+8801615298449",//Your Number
-  admin: ["5920596290", "8179590682"], // your id
+  ownerUsernames: ["Arafat"],//Your Username
+  ownerNumber: "+8801878266244",//Your Number
+  admin: ["7326996884"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "token",//Your Bot Tokens
-  imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
+  telegramBotToken: "7919475686:AAG45pU6R962dfdDeMkpysODLd9tXEHl4po",//Your Bot Tokens
+  imageUrl: "https://i.postimg.cc/7Lbh7QF6/received-517669337949724.jpg",//Thumbnail Url
   po0rt: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    ownerlink: "https://t.me/Arafatas602",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
